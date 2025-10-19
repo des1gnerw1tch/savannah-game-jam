7a9ea5c9be1bbfd9615a33eeec3e4e214111380c
des1gnerw1tch <zachcanon@gmail.com>
2025-10-19 19:25:50 -0400

    Obstacle spawning in runner

---
+++ Savannah/Assets/Scripts/Bounds.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+    [SerializeField] private float width;
+    [SerializeField] private float length;
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Vector3 center = new Vector3(transform.position.x, transform.position.y, transform.position.z - (length / 2));
+        Gizmos.DrawWireCube(center, new Vector3(width, 1, length));
+    }
+
+    public float GetMaxX() => this.transform.position.x + (width / 2);
+    public float GetMinX() => this.transform.position.x - (width / 2);
+    public float GetMaxZ() => this.transform.position.z;
+    public float GetMinZ() => this.transform.position.z - length;
+}
