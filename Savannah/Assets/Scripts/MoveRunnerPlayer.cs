7e7ded547723892f8ec79b03399ce8ce00cfdc97
des1gnerw1tch <zachcanon@gmail.com>
2025-10-19 20:14:03 -0400

    On collision player logs

--- Savannah/Assets/Scripts/MoveRunnerPlayer.cs
+++ Savannah/Assets/Scripts/MoveRunnerPlayer.cs
@@ -1,11 +1,17 @@
+using System;
 using UnityEngine;
 
 public class MoveRunnerPlayer : MonoBehaviour
 {
     [SerializeField] private float speed;
-
+    
     void Update()
     {
         transform.position += this.transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision!");
     }
 }
