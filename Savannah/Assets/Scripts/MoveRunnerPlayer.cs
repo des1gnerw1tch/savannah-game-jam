be57906b59ae788bd17cfd1f0b3d1b765c52e632
des1gnerw1tch <zachcanon@gmail.com>
2025-10-19 20:42:12 -0400

    Added incrementing speed

--- Savannah/Assets/Scripts/MoveRunnerPlayer.cs
+++ Savannah/Assets/Scripts/MoveRunnerPlayer.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 public class MoveRunnerPlayer : MonoBehaviour
 {
     [SerializeField] private float speed;
-    
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float speedIncrementWhenPressMouseButton;
+
     void Update()
     {
         transform.position += this.transform.forward * speed * Time.deltaTime;
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.speed += speedIncrementWhenPressMouseButton;
+            Debug.Log("Left mouse button down");
+            ClampSpeed();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.speed -= speedIncrementWhenPressMouseButton;
+            Debug.Log("Right mouse button down");
+            ClampSpeed();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collision!");
+    }
+
+    private void ClampSpeed()
+    {
+        this.speed = Mathf.Clamp(speed, 0, maxSpeed);
     }
 }
